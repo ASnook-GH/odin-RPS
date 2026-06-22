b7705aed6409ba8b842ae820682494b407929547
Andrew Snook <260702288+ASnook-GH@users.noreply.github.com>
2026-06-22 13:57:42 +0200

    wrote the computer's logic for its choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice(val) {
+  let value = Math.floor(Math.random() * val);
+  if (value === 1) {
+    return "Rock";
+  } else if (value === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+console.log(getComputerChoice(3));
+console.log(getComputerChoice(3));
+console.log(getComputerChoice(3));
