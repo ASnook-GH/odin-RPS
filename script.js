5dcd24097cad2589610a573be656cf8a4f7f8ef2
Andrew Snook <260702288+ASnook-GH@users.noreply.github.com>
2026-06-22 18:29:55 +0200

    added player score variables & started working on game round logic

--- script.js
+++ script.js
@@ -11,5 +11,29 @@ function getComputerChoice(val) {
 
 function getPlayerChoice() {
   let choice = prompt("Rock, Paper, Scissors ? ");
-  return choice.toLowerCase;
+  return choice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, playerChoice) {
+  if (computerChoice === "Rock" && playerChoice === "scissors") {
+    return "Computer wins, Rock beats Scissors";
+  } else if (computerChoice === "Rock" && playerChoice === "paper") {
+    return "Player wins, Paper beats Rock";
+  } else if (computerChoice === "Scissors" && playerChoice === "paper") {
+    return "Computer wins, Scissors beat paper";
+  } else if (computerChoice === "Scissors" && playerChoice === "rock") {
+    return "Player wins, Rock beats Scissors";
+  } else if (computerChoice === "Paper" && playerChoice === "rock") {
+    return "Computer wins, Paper beats Rock";
+  } else if (computerChoice === "Paper" && playerChoice === "scissors") {
+    return "Player wins, Scissors beats Paper";
+  } else {
+    return "It's a draw";
+  }
+
+  playerChoice = getPlayerChoice();
+  computerChoice = getComputerChoice(3);
 }
