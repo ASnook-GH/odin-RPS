ede1508973cbfecd86e5659d91d88782b48c4c0d
Andrew Snook <260702288+ASnook-GH@users.noreply.github.com>
2026-06-23 11:21:38 +0200

    added the logic for one round of play

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getComputerChoice(val) {
   let value = Math.floor(Math.random() * val);
   if (value === 1) {
-    return "Rock";
+    return "rock";
   } else if (value === 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
@@ -14,26 +14,43 @@ function getPlayerChoice() {
   return choice;
 }
 
+// console.log(getPlayerChoice());
+// console.log(getComputerChoice(3));
+
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(computerChoice, playerChoice) {
-  if (computerChoice === "Rock" && playerChoice === "scissors") {
-    return "Computer wins, Rock beats Scissors";
-  } else if (computerChoice === "Rock" && playerChoice === "paper") {
-    return "Player wins, Paper beats Rock";
-  } else if (computerChoice === "Scissors" && playerChoice === "paper") {
-    return "Computer wins, Scissors beat paper";
-  } else if (computerChoice === "Scissors" && playerChoice === "rock") {
-    return "Player wins, Rock beats Scissors";
-  } else if (computerChoice === "Paper" && playerChoice === "rock") {
-    return "Computer wins, Paper beats Rock";
-  } else if (computerChoice === "Paper" && playerChoice === "scissors") {
-    return "Player wins, Scissors beats Paper";
+const playerChoice = getPlayerChoice().toLowerCase();
+const computerChoice = getComputerChoice(3);
+
+function playRound(playerChoice, computerChoice) {
+  if (computerChoice === "rock" && playerChoice === "scissors") {
+    computerScore += 1;
+    return `Computer wins, Rock beats Scissors 
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+  } else if (computerChoice === "rock" && playerChoice === "paper") {
+    humanScore += 1;
+    return `Player wins, Paper beats Rock
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+  } else if (computerChoice === "scissors" && playerChoice === "paper") {
+    computerScore += 1;
+    return `Computer wins, Scissors beat paper
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+  } else if (computerChoice === "scissors" && playerChoice === "rock") {
+    humanScore += 1;
+    return `Player wins, Rock beats Scissors
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+  } else if (computerChoice === "paper" && playerChoice === "rock") {
+    computerScore += 1;
+    return `Computer wins, Paper beats Rock
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+  } else if (computerChoice === "paper" && playerChoice === "scissors") {
+    humanScore += 1;
+    return `Player wins, Scissors beats Paper
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
   } else {
     return "It's a draw";
   }
-
-  playerChoice = getPlayerChoice();
-  computerChoice = getComputerChoice(3);
 }
+
+console.log(playRound(playerChoice, computerChoice));
