c2386d6e014607e7449e60b908c93fc586ac9d70
Andrew Snook <260702288+ASnook-GH@users.noreply.github.com>
2026-06-23 15:00:17 +0200

    updated the game logic & finished project

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function getComputerChoice(val) {
-  let value = Math.floor(Math.random() * val);
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let value = Math.floor(Math.random() * 3);
   if (value === 1) {
     return "rock";
   } else if (value === 2) {
@@ -15,42 +18,76 @@ function getPlayerChoice() {
 }
 
 // console.log(getPlayerChoice());
-// console.log(getComputerChoice(3));
+// console.log(getComputerChoice());
 
-let humanScore = 0;
-let computerScore = 0;
-
-const playerChoice = getPlayerChoice().toLowerCase();
-const computerChoice = getComputerChoice(3);
-
-function playRound(playerChoice, computerChoice) {
+// function playRound(playerChoice, computerChoice) {
+function playRound() {
+  let playerChoice = getPlayerChoice().toLowerCase();
+  let computerChoice = getComputerChoice();
+  console.log("playerchoice = " + playerChoice);
+  console.log("computerChoice = " + computerChoice);
   if (computerChoice === "rock" && playerChoice === "scissors") {
     computerScore += 1;
-    return `Computer wins, Rock beats Scissors 
-      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+    console.log(`Computer wins, Rock beats Scissors 
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`);
   } else if (computerChoice === "rock" && playerChoice === "paper") {
     humanScore += 1;
-    return `Player wins, Paper beats Rock
-      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+    console.log(`Player wins, Paper beats Rock
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`);
   } else if (computerChoice === "scissors" && playerChoice === "paper") {
     computerScore += 1;
-    return `Computer wins, Scissors beat paper
-      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+    console.log(`Computer wins, Scissors beat paper
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`);
   } else if (computerChoice === "scissors" && playerChoice === "rock") {
     humanScore += 1;
-    return `Player wins, Rock beats Scissors
-      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+    console.log(`Player wins, Rock beats Scissors
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`);
   } else if (computerChoice === "paper" && playerChoice === "rock") {
     computerScore += 1;
-    return `Computer wins, Paper beats Rock
-      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+    console.log(`Computer wins, Paper beats Rock
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`);
   } else if (computerChoice === "paper" && playerChoice === "scissors") {
     humanScore += 1;
-    return `Player wins, Scissors beats Paper
-      Current score : Computer : ${computerScore} - Human : ${humanScore}`;
+    console.log(`Player wins, Scissors beats Paper
+      Current score : Computer : ${computerScore} - Human : ${humanScore}`);
   } else {
-    return "It's a draw";
+    console.log("It's a draw");
   }
+  console.log("end playround");
 }
 
-console.log(playRound(playerChoice, computerChoice));
+// console.log(playRound(playerChoice, computerChoice));
+
+// function playGame() {
+//   playRound(playerChoice, computerChoice);
+
+//   if (humanScore >= 3) {
+//     console.log("Congratulations ! You won");
+//   } else if (computerScore >= 3) {
+//     console.log("Sorry ! you lost");
+//   } else {
+//     alert("Another round ?");
+//     playRound(playerChoice, computerChoice);
+//   }
+// }
+
+function playGame2() {
+  let round = 0;
+  while (humanScore < 5 || computerScore < 5) {
+    round++;
+    console.log(round);
+    playRound();
+    console.log(
+      `the score is the following : Computer : ${computerScore} - Human : ${humanScore}`,
+    );
+    if (round === 13) {
+      break;
+      console.log(
+        `It's round ${round} and the score is the following : Computer : ${computerScore} - Human : ${humanScore}`,
+      );
+    }
+  }
+  console.log("end prog");
+}
+
+playGame2();
